894b7a1d5ba61134babc2d24b4ffc87d6763f9fe
Mehmet Eyüp Katırcı <mehmeteyupkatirci@gmail.com>
2020-06-08 20:21:00 +0300

    ArtistDalTest

--- GPR.Laterna.DataAccess.Tests/ArtistDalTests.cs
+++ GPR.Laterna.DataAccess.Tests/ArtistDalTests.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using GPR.Laterna.DataAccess.Abstract;
 using GPR.Laterna.Business.Concrete;
+using GPR.Laterna.Entities.Concrete;
 
 namespace GPR.Laterna.DataAccess.Tests
 {
@@ -17,9 +18,46 @@ namespace GPR.Laterna.DataAccess.Tests
         }
 
         [TestMethod]
-        public void TestMethod1()
+        public void AddArtistTest()
         {
-            
+            var addedArtist = _artistDal.Add(new Artist
+            {
+                Name = "DAL.Artist.Test",
+                SpotId = "aklala",
+                Popularity = 12,
+                CreatedAt = DateTime.Now,
+                Genres = "Deneme türü",
+            });
+        }
+
+        [TestMethod]
+        public void DeleteArtistTest()
+        {
+            var testArtist = _artistDal.Get(x => x.Name == "DAL.Artist.Test");
+            if (testArtist != null)
+            {
+                _artistDal.Delete(testArtist);
+            }
+            else
+            {
+                Assert.AreEqual(1, 1);
+            }
+        }
+
+        [TestMethod]
+        public void GetOneArtistTest()
+        {
+            var playlist = _artistDal.Get(x => x.Id == 1);
+
+            Assert.IsNotNull(playlist);
+        }
+
+        [TestMethod]
+        public void GetArtistListTest()
+        {
+            var list = _artistDal.GetList();
+
+            Assert.IsTrue(list.Count > 0);
         }
     }
 }
