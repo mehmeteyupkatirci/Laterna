520db995d102ce4187b4df09d53dc96462dd81ce
Mehmet Eyüp Katırcı <mehmeteyupkatirci@gmail.com>
2020-06-08 20:29:23 +0300

    ArtistDalTest

--- GPR.Laterna.DataAccess.Tests/PlaylistTrackDalTests.cs
+++ GPR.Laterna.DataAccess.Tests/PlaylistTrackDalTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using GPR.Laterna.Business.Concrete;
 using GPR.Laterna.DataAccess.Abstract;
+using GPR.Laterna.Entities.Concrete;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace GPR.Laterna.DataAccess.Tests
@@ -14,8 +15,44 @@ namespace GPR.Laterna.DataAccess.Tests
             _playlistTrackDal = DalFactory.CreatePlaylistTrackDal();
         }
         [TestMethod]
-        public void TestMethod1()
+        public void AddPlaylistTrackTest()
         {
+            var addedPlaylistTrack = _playlistTrackDal.Add(new PlaylistTrack
+            {
+                PlaylistId = 10011,
+                TrackId = 11317,
+                CreatedAt = DateTime.Now,
+                UpdatedAt = DateTime.Now
+            });
+        }
+        [TestMethod]
+        public void DeletePlaylistTrackTest()
+        {
+            var testPlaylist = _playlistTrackDal.Get(x => x.PlaylistId == 10011 && x.TrackId == 11317);
+            if (testPlaylist != null)
+            {
+                _playlistTrackDal.Delete(testPlaylist);
+            }
+            else
+            {
+                Assert.AreEqual(1, 1);
+            }
+        }
+
+        [TestMethod]
+        public void GetOnePlaylistTrackTest()
+        {
+            var playlist = _playlistTrackDal.Get(x => x.PlaylistId == 10011 && x.TrackId == 100);
+
+            Assert.IsNotNull(playlist);
+        }
+
+        [TestMethod]
+        public void GetPlaylistTrackListTest()
+        {
+            var list = _playlistTrackDal.GetList();
+
+            Assert.IsTrue(list.Count > 0);
         }
     }
 }
